5aa1680b683962225e86bc37094f2d0c66abdf20
Sultan Fariz <sultan_fariz@apps.ipb.ac.id>
2023-04-02 14:16:39 +0700

    feat: add graphql

---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,17 @@
+module Types
+  class QueryType < Types::BaseObject
+    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
+    include GraphQL::Types::Relay::HasNodeField
+    include GraphQL::Types::Relay::HasNodesField
+
+    # Add root-level fields here.
+    # They will be entry points for queries on your schema.
+
+    # TODO: remove me
+    field :users, String, null: false,
+      description: "An example field added by the generator"
+    def users
+      "Hello World!"
+    end
+  end
+end
